b9639ae16bd49dec165a530e2d94ffd0b78a69c3
UserTeemu <20520700+UserTeemu@users.noreply.github.com>
2021-02-06 16:00:15 +0200

    Removed the class transformer debug feature that doesn't work anyway

--- src/main/java/io/github/tivj/f5transitions/asm/tweaker/ClassTransformer.java
+++ src/main/java/io/github/tivj/f5transitions/asm/tweaker/ClassTransformer.java
@@ -9,38 +9,35 @@ import io.github.tivj.f5transitions.asm.tweaker.transformer.ITransformer;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.util.CheckClassAdapter;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class ClassTransformer implements IClassTransformer {
     private final Multimap<String, ITransformer> transformerMap = ArrayListMultimap.create();
-    private static final boolean debug = false;
     private static final Logger logger = TransitionsMod.LOGGER;
 
     public ClassTransformer() {
         registerTransformer(new EntityRendererTransformer());
-        registerTransformer(new LayerHeldItemTransformer());
         registerTransformer(new MinecraftTransformer());
-        registerTransformer(new RendererLivingEntityTransformer());
         registerTransformer(new RenderGlobalTransformer());
         registerTransformer(new ItemRendererTransformer());
+
+        // opacity transformers
+        registerTransformer(new LayerHeldItemTransformer());
+        registerTransformer(new RendererLivingEntityTransformer());
         registerTransformer(new LayerArmorBaseTransformer());
         registerTransformer(new LayerCustomHeadTransformer());
         registerTransformer(new LayerCapeTransformer());
         registerTransformer(new LayerDeadmau5HeadTransformer());
 
+        // block aabb provider transformers
         registerTransformer(new BlockAABBProviderTransformer.BlockTransformer());
         registerTransformer(new BlockAABBProviderTransformer.BlockStairsTransformer());
         registerTransformer(new BlockAABBProviderTransformer.PlaceholderMethodReplacerTransformer());
 
+        //debug transformer
         registerTransformer(new GuiOverlayDebugTransformerForDebug());
     }
 
@@ -75,37 +72,6 @@ public class ClassTransformer implements IClassTransformer {
         } catch (Throwable t) {
             logger.error("Exception when transforming " + transformedName + ": " + t.getClass().getSimpleName());
             t.printStackTrace();
-        }
-
-        if (debug) {
-            List<String> debuggableMethods = new ArrayList<>();
-            for (ITransformer transformer : transformers) {
-                debuggableMethods.addAll(transformer.debuggableClass());
-            }
-
-            if (debuggableMethods.size() > 0) {
-                logger.info("Debugger is visiting class "+node.name+"...");
-                try {
-                    ClassVisitor visitor = new CheckClassAdapter(writer);
-                    visitor.visit(node.version, node.access, node.name, node.signature, node.superName, node.interfaces.toArray(new String[0]));
-                    for (MethodNode method : node.methods) {
-                        if (debuggableMethods.contains(method.name)) {
-                            logger.info("Debugger is visiting method " + node.name + "#" + method.name + "...");
-                            try {
-                                MethodVisitor methodVisitor = visitor.visitMethod(method.access, method.name, method.desc, method.signature, method.exceptions.toArray(new String[0]));
-                                methodVisitor.visitCode();
-                                methodVisitor.visitEnd();
-                            } catch (Throwable e) {
-                                logger.error("Error while visiting method "+ node.name + "#" + method.name, e);
-                            }
-                            logger.info("Method " + node.name + "#" + method.name + " has been visited by the debugger.");
-                        }
-                    }
-                } catch (Throwable e) {
-                    logger.error("Error while visiting class "+node.name, e);
-                }
-                logger.info("Class "+node.name+" has been visited by the debugger.");
-            }
         }
 
         return writer.toByteArray();
